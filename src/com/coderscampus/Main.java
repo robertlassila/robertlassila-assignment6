c0a3aa59ae6c38a1ac8c7af33edf9ebf7473c8df
robertlassila <rlassila42@gmail.com>
2025-01-08 11:22:40 -0800

    Created methods for Models S and X
    Copy and pasted the method for the Model 3, then made adjustments so it worked properly.
    Also removed unused variables

--- src/com/coderscampus/Main.java
+++ src/com/coderscampus/Main.java
@@ -15,12 +15,16 @@ public class Main {
 		
 		FileService fileService = new FileService();
 		fileService.readFile("model3.csv", monthlySalesList);
-		findAndPrintSalesInfo(monthlySalesList);
+		findAndPrintSalesInfoModel3(monthlySalesList);
+		fileService.readFile("modelS.csv", monthlySalesList);
+		findAndPrintSalesInfoModelS(monthlySalesList);
+		fileService.readFile("modelX.csv", monthlySalesList);
+		findAndPrintSalesInfoModelX(monthlySalesList);
 		
 		//System.out.println(monthlySalesList.toString());
 
 	}
-	public static void findAndPrintSalesInfo(List<MonthlySale> monthlySalesList) {
+	public static void findAndPrintSalesInfoModel3(List<MonthlySale> monthlySalesList) {
 		
 		System.out.println("Model 3 Yearly Sales Report");
 		System.out.println("---------------");
@@ -43,18 +47,11 @@ public class Main {
 				.sum();
 		System.out.println("2019 -> " + totalSales2019);
 		
-		DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yy-MMM");
-        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("yyyy-MM");
-		
 		Optional<MonthlySale> maxSalesObjectModel3 = monthlySalesList.stream()
 				.max(Comparator.comparingInt(MonthlySale::getSales));
 		
 		Optional<MonthlySale> minSalesObjectModel3 = monthlySalesList.stream()
 				.min(Comparator.comparingInt(MonthlySale::getSales));
-		
-		
-		
-		//System.out.println(maxSalesObjectModel3);
 		
 		maxSalesObjectModel3.ifPresent(sales -> System.out.println("The best month for Model 3 was " + 
 							LocalDate.parse("01-" + sales.getDate(), DateTimeFormatter.ofPattern("dd-MMM-yy"))
@@ -63,11 +60,110 @@ public class Main {
 		minSalesObjectModel3.ifPresent(sales -> System.out.println("The worst month for Model 3 was " + 
 				LocalDate.parse("01-" + sales.getDate(), DateTimeFormatter.ofPattern("dd-MMM-yy"))
 				.format(DateTimeFormatter.ofPattern("yyyy-MM"))));
-//		
-//		Optional<MonthlySale> maxSalesObject18 = monthlySalesList.stream()
-//				.filter(monthlySale -> monthlySale.getDate().contains("18"))
-//				.max(Comparator.comparingInt(MonthlySale::getSales));
+		
+		System.out.println();
+		
+		monthlySalesList.clear();
 		
 	}
+	
+public static void findAndPrintSalesInfoModelS(List<MonthlySale> monthlySalesList) {
+		
+		System.out.println("Model S Yearly Sales Report");
+		System.out.println("---------------");
+		
+		int totalSales2016 = monthlySalesList.stream()
+				.filter(sales -> sales.getDate().contains("16"))
+				.mapToInt(MonthlySale::getSales)
+				.sum();
+		System.out.println("2016 -> " + totalSales2016);
+		
+		int totalSales2017 = monthlySalesList.stream()
+							.filter(sales -> sales.getDate().contains("17"))
+							.mapToInt(MonthlySale::getSales)
+							.sum();
+		System.out.println("2017 -> " + totalSales2017);
+		
+		int totalSales2018 = monthlySalesList.stream()
+				.filter(sales -> sales.getDate().contains("18"))
+				.mapToInt(MonthlySale::getSales)
+				.sum();
+		System.out.println("2018 -> " + totalSales2018);
+		
+		int totalSales2019 = monthlySalesList.stream()
+				.filter(sales -> sales.getDate().contains("19"))
+				.mapToInt(MonthlySale::getSales)
+				.sum();
+		System.out.println("2019 -> " + totalSales2019);
+
+		Optional<MonthlySale> maxSalesObjectModel3 = monthlySalesList.stream()
+				.max(Comparator.comparingInt(MonthlySale::getSales));
+		
+		Optional<MonthlySale> minSalesObjectModel3 = monthlySalesList.stream()
+				.min(Comparator.comparingInt(MonthlySale::getSales));
+		
+		maxSalesObjectModel3.ifPresent(sales -> System.out.println("The best month for Model S was " + 
+							LocalDate.parse("01-" + sales.getDate(), DateTimeFormatter.ofPattern("dd-MMM-yy"))
+							.format(DateTimeFormatter.ofPattern("yyyy-MM"))));
+		
+		minSalesObjectModel3.ifPresent(sales -> System.out.println("The worst month for Model S was " + 
+				LocalDate.parse("01-" + sales.getDate(), DateTimeFormatter.ofPattern("dd-MMM-yy"))
+				.format(DateTimeFormatter.ofPattern("yyyy-MM"))));
+		
+		System.out.println();
+		
+		monthlySalesList.clear();
+		
+	}
+
+public static void findAndPrintSalesInfoModelX(List<MonthlySale> monthlySalesList) {
+	
+	System.out.println("Model X Yearly Sales Report");
+	System.out.println("---------------");
+	
+	int totalSales2016 = monthlySalesList.stream()
+			.filter(sales -> sales.getDate().contains("16"))
+			.mapToInt(MonthlySale::getSales)
+			.sum();
+	System.out.println("2016 -> " + totalSales2016);
+	
+	int totalSales2017 = monthlySalesList.stream()
+						.filter(sales -> sales.getDate().contains("17"))
+						.mapToInt(MonthlySale::getSales)
+						.sum();
+	System.out.println("2017 -> " + totalSales2017);
+	
+	int totalSales2018 = monthlySalesList.stream()
+			.filter(sales -> sales.getDate().contains("18"))
+			.mapToInt(MonthlySale::getSales)
+			.sum();
+	System.out.println("2018 -> " + totalSales2018);
+	
+	int totalSales2019 = monthlySalesList.stream()
+			.filter(sales -> sales.getDate().contains("19"))
+			.mapToInt(MonthlySale::getSales)
+			.sum();
+	System.out.println("2019 -> " + totalSales2019);
+
+	
+	Optional<MonthlySale> maxSalesObjectModel3 = monthlySalesList.stream()
+			.max(Comparator.comparingInt(MonthlySale::getSales));
+	
+	Optional<MonthlySale> minSalesObjectModel3 = monthlySalesList.stream()
+			.min(Comparator.comparingInt(MonthlySale::getSales));
+	
+	maxSalesObjectModel3.ifPresent(sales -> System.out.println("The best month for Model X was " + 
+						LocalDate.parse("01-" + sales.getDate(), DateTimeFormatter.ofPattern("dd-MMM-yy"))
+						.format(DateTimeFormatter.ofPattern("yyyy-MM"))));
+	
+	minSalesObjectModel3.ifPresent(sales -> System.out.println("The worst month for Model X was " + 
+			LocalDate.parse("01-" + sales.getDate(), DateTimeFormatter.ofPattern("dd-MMM-yy"))
+			.format(DateTimeFormatter.ofPattern("yyyy-MM"))));
+	
+	System.out.println();
+	
+	monthlySalesList.clear();
+	
+}
 
 }
